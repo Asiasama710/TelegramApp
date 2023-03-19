74edef722aff139538d900d387e70656e25d145d
Asia sama <sehunexo710@gmail.com>
2023-03-19 15:37:06 +0300

    happy commit

---
+++ app/src/main/java/com/asiasama/clone_telegram_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.asiasama.clone_telegram_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
